59891bf65c46aa03de5006b0fa00b24921d5861a
Steve Helinski <helinskis@gmail.com>
2026-01-19 14:49:35 -0500

    Test

---
+++ oxygen-webhelp/search/keywords.js
@@ -0,0 +1,5 @@
+var keywords=[{w:"Opening",p:["p0","p1"]},{w:"the",p:["p0","p1"]},{w:"Square",p:["p0"]},{w:"POS",p:["p0"]},{w:"and",p:["p0"]},{w:"Cash",p:["p0"]},{w:"Register",p:["p0"]},{w:"Winery",p:["p1"]},{w:"Facility",p:["p1"]},{w:"Welcome",p:["p2"]}];
+var ph={};
+ph["p0"]=[0, 1, 2, 3, 4, 5, 6];
+ph["p1"]=[0, 1, 7, 8];
+ph["p2"]=[9];
